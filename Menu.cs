9d5f7e3c5a466266c1613dc55e862ebcb07a7041
Hum-jr <bennyangiye202@gmail.com>
2024-09-08 22:59:21 +0300

    First Updates

--- Menu.cs
+++ Menu.cs
@@ -7,24 +7,48 @@ using App;
 public partial class Menu : Control
 {
 	// Called when the node enters the scene tree for the first time.
+	
+	Budget currentBudget;
 	public override void _Ready()
 	{
-		Budget currentBudget = new Budget();
-		currentBudget.Name = "Current Budget";
-		currentBudget.Expenses = new List<Transaction>();
-		currentBudget.Income = new List<Transaction>();
+		currentBudget = new Budget()
+		{
+			Name = "Current Budget",
+			Expenses = new List<Transaction>(),
+			Income = new List<Transaction>(),
+		};
+
+		currentBudget.Expenses.Add(new Transaction()
+		{
+			Name = "Test",
+			Date = DateTime.Now,
+			Amount = 30,
+			Type = "none"
+		});
 		
 		currentBudget.Expenses.Add(new Transaction()
 		{
-				Name = "Test",
-				Date = DateTime.Now(),
-				Amount = 30,
-				Type = "none"
+			Name = "Test 1",
+			Date = DateTime.Now,
+			Amount = 30,
+			Type = "none"
+		});
+		
+		currentBudget.Expenses.Add(new Transaction()
+		{
+			Name = "Test 2",
+			Date = DateTime.Now,
+			Amount = 30,
+			Type = "none"
 		});
 	}
+
+
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		GD.Print(currentBudget.Name);
+		GD.Print(currentBudget.Expenses[0].Name);
 	}
 }
