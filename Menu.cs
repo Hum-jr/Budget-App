87bc48172cfcc023e9bc08f2f74fa8ec165ddfb4
Hum-jr <bennyangiye202@gmail.com>
2024-09-23 20:57:36 +0300

    push

--- Menu.cs
+++ Menu.cs
@@ -9,6 +9,9 @@ public partial class Menu : Control
 	// Called when the node enters the scene tree for the first time.
 	
 	Budget currentBudget;
+	
+	[Export]
+	public PackedScene TransactionList;
 	public override void _Ready()
 	{
 		currentBudget = new Budget()
@@ -41,6 +44,17 @@ public partial class Menu : Control
 			Amount = 30,
 			Type = "none"
 		});
+
+
+		VBoxContainer container = GetNode<VBoxContainer>("ScrollContainer/TransactionList");
+		
+		Node tableRow = TransactionList.Instantiate();
+		tableRow.GetNode<RichTextLabel>("Name").Text = currentBudget.Expenses[0].Name;
+		tableRow.GetNode<RichTextLabel>("Date").Text = currentBudget.Expenses[0].Date.ToString();
+		tableRow.GetNode<RichTextLabel>("Amount").Text = currentBudget.Expenses[0].Amount.ToString();
+		tableRow.GetNode<RichTextLabel>("Type").Text = currentBudget.Expenses[0].Type;
+		container.AddChild(tableRow);
+		
 	}
 
 
